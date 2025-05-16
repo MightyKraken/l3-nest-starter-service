c44345a2564488fc0e7a71aaab0a01d88a0ff5c4
Sabik Abtahee <sabik.abtahee@gmail.com>
2025-05-16 22:27:42 +0600

    chore: experimental

--- src/controller/controller.controller.ts
+++ src/controller/controller.controller.ts
@@ -1,4 +1,17 @@
-import { Controller, Get, HttpCode } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	Param,
+	ParamData,
+	Post,
+	Req,
+	Res
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('controller')
 export class ControllerController {
@@ -27,5 +40,46 @@ export class ControllerController {
 	@HttpCode(500)
 	getError500(): object {
 		return { error: 'should return 500' };
+	}
+
+	@Get('response')
+	getResponse(@Res() res: Response): object {
+		const test = res.status(200).json({ a: 2 });
+		return test;
+	}
+
+	@Get('request/:id')
+	getRequest(@Req() req: Request, @Param('id') id: ParamData): object {
+		const body: unknown = req.body;
+		const params: any = req.params;
+		const queryParam: any = req.query;
+		const headers: any = req.headers;
+		const ip: any = req.ip;
+		const host: any = req.host;
+
+		console.log('asdasd', ip);
+
+		console.log(host);
+
+		console.log(headers);
+
+		console.log(id);
+
+		console.log(body);
+		console.log(queryParam);
+		console.log(params);
+
+		return { test: 'a' };
+	}
+
+	@Post()
+	async create(@Body() createCatDto: CreateCatDto): Promise<string> {
+		console.log(createCatDto);
+		return await Promise.resolve('This action adds a new cat');
+	}
+
+	@Get('wildcard/*')
+	wildCard(): string {
+		return 'This route uses a wildcard';
 	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestFastifyApplication } from '@nestjs/platform-fastify';
 
@@ -5,6 +6,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create<NestFastifyApplication>(AppModule);
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true
+		})
+	);
 	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap()
