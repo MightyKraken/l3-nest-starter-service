fe1ba0dbc171dca723f052bb1a758a334e1b978c
SABIK\sabik <sabik.abtahee@gmail.com>
2025-07-03 22:53:52 +0600

    chore: env added in yml

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,7 +1,10 @@
+import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+
+const mockJwtService = {};
 
 describe('AuthController', () => {
 	let controller: AuthController;
@@ -16,6 +19,10 @@ describe('AuthController', () => {
 						login: jest.fn(),
 						signUp: jest.fn()
 					}
+				},
+				{
+					provide: JwtService,
+					useValue: mockJwtService
 				}
 			]
 		}).compile();
